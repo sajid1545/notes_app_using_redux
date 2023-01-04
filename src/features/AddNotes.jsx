4f3dc0f51463a3453bae75189b5887b911431126
sajid9962 <clashking1545@gmail.com>
2023-01-04 20:27:15 +0600

    store created

--- src/features/AddNotes.jsx
+++ src/features/AddNotes.jsx
@@ -5,11 +5,13 @@ const AddNotes = () => {
 	const [notes, setNotes] = useState('');
 	const [title, setTitle] = useState('');
 
+
+	
 	return (
 		<div>
 			<h1 className="text-7xl text-center font-extrabold">Add Notes</h1>
 
-			<form action="" className="my-10 border-4 p-20 w-[90%] mx-auto">
+			<form action="" className="my-10 border-4 border-gray-600 rounded-lg shadow-2xl hover:shadow-blue-300 duration-500 p-20 w-[90%] mx-auto">
 				<Box className="w-[50%] mx-auto space-y-10">
 					<TextField
 						id="outlined-basic"
--- src/features/notesSlice.js
+++ src/features/notesSlice.js
@@ -6,17 +6,24 @@ const initialNoteState = {
 		{
 			id: uuidv4(),
 			title: 'React',
-			author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, id.',
+			author:
+				'React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.',
 		},
 		{
 			id: uuidv4(),
 			title: 'JavaScript',
-			note: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, id.',
+			note: 'JavaScript is the worlds most popular programming language. JavaScript is the programming language of the Web',
 		},
 	],
 };
 
 const noteSlice = createSlice({
 	name: 'notes',
+	initialState: initialNoteState,
+	reducers: {
+		showNotes: (state) => state,
+	},
 });
 
+export const { showNotes } = noteSlice.actions;
+export default noteSlice.reducer;
