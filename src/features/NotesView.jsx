1c2e0e66175a8183c2a59e986095d435c2009852
sajid9962 <clashking1545@gmail.com>
2023-01-04 21:41:17 +0600

    edit and delete button added

--- src/features/NotesView.jsx
+++ src/features/NotesView.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { FaTrash } from 'react-icons/fa';
+import { FiEdit } from 'react-icons/fi';
 
 const NotesView = () => {
-	
 	const notes = useSelector((state) => state.notesReducer.notes);
 
 	return (
 		<div>
 			<h1 className="text-7xl text-center font-extrabold">All Notes</h1>
 
-			{notes && notes.map((n) => {
-				const { id, title, note } = n;
-				console.log(n);
-				return (
-					<div
-						key={id}
-						className="rounded-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 my-10 shadow-xl w-2/4 mx-auto p-5">
-						<div className="block rounded-xl bg-white p-6 sm:p-8">
-							<div className="flex flex-col justify-center items-center sm:pr-8">
-								<h3 className="text-xl font-bold text-gray-900">{title}</h3>
+			{notes &&
+				notes.map((n) => {
+					const { id, title, note } = n;
+					return (
+						<div
+							key={id}
+							className="rounded-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 my-10 shadow-xl w-2/4 mx-auto p-5">
+							<div className="block rounded-xl bg-white p-6 sm:p-8">
+								<div className="flex flex-col justify-center items-center sm:pr-8">
+									<h3 className="text-4xl font-extrabold text-gray-900">{title}</h3>
 
-								<p className="mt-2 text-md text-gray-800">{note}</p>
+									<p className="mt-4 text-lg text-gray-800">{note}</p>
+								</div>
+								<div className='mt-5 flex justify-end gap-5'>
+									<button
+										type="button"
+										className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  w-12 h-12 rounded-lg ">
+										<FaTrash />
+									</button>
+									<button
+										type="button"
+										className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  w-12 h-12 rounded-lg ">
+										<FiEdit />
+									</button>
+								</div>
 							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
 		</div>
 	);
 };
